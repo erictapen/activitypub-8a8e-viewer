841def2c6e09aa77e46877fc3f31fd5efc382233
Kerstin Humm <kerstin@erictapen.name>
2025-06-07 18:27:00 +0200

    show content of attr keys

--- src/index.ts
+++ src/index.ts
@@ -3,9 +3,10 @@ export function handleApUrl(_: Event) {
   let input = document.getElementById("ap-input") as HTMLInputElement;
   let searchResult = document.getElementById("searchresult") as HTMLElement;
   fetch(input.value, { headers: { "Accept": "application/activity+json"}}).then(async response => {
+    const obj = await response.json();
     searchResult.innerHTML = "<ul>";
-    for (const key in await response.json()) {
-      searchResult.innerHTML += `<li>${key}</li>`;
+    for (const key in obj) {
+      searchResult.innerHTML += `<li>${key}: ${JSON.stringify(obj[key])}</li>`;
     }
     searchResult.innerHTML += "</ul>";
   });
